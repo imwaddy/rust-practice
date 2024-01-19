3208f46509e7b24ef0c146af577f6234303c8b69
Mayur Wadekar <mayurwadekar2@gmail.com>
2024-01-19 08:34:37 +0530

    removed target folder

--- hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643.d
+++
@@ -1,5 +0,0 @@
-/Users/mayurwadekar/go/src/rust-practice/hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643: src/main.rs
-
-/Users/mayurwadekar/go/src/rust-practice/hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/mayurwadekar/go/src/rust-practice/hello_world/target/debug/hello_world: /Users/mayurwadekar/go/src/rust-practice/hello_world/src/main.rs
